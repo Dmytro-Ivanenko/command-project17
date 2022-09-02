c741fee7038add6b6ae74b90d3d87cb36970907a
Dmytro-Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2022-09-02 20:23:57 +0000

    Deploying to gh-pages from @ Dmytro-Ivanenko/command-project17@3ee3c94295f10973b01c23c7850a1ad642fa1c30 🚀

---
+++ index.8cc0f192.js
@@ -0,0 +1,2 @@
+$(".slider-reviews").slick({infinite:!1,dots:!0,arrows:!1,slidesToShow:1});
+//# sourceMappingURL=index.8cc0f192.js.map
---
+++ index.f27853b9.js
@@ -0,0 +1,2 @@
+$(".slider-reviews").slick({infinite:!1,dots:!0,arrows:!1,slidesToShow:1});
+//# sourceMappingURL=index.f27853b9.js.map
